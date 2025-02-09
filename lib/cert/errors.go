a7a96ebf721dee627fbf4a0cc9fe3d70cc175501
snowmerak <snowmerak@outlook.com>
2025-02-09 19:25:30 +0900

    add key is too short error

--- lib/cert/errors.go
+++ lib/cert/errors.go
@@ -7,3 +7,11 @@ var masterKeyIsNotSetError = errors.New("master key is not set")
 func IsMasterKeyIsNotSetError(err error) bool {
 	return errors.Is(err, masterKeyIsNotSetError)
 }
+
+var (
+	keyIsTooShortError = errors.New("key length is less than needed bytes")
+)
+
+func IsKeyIsTooShortError(err error) bool {
+	return errors.Is(err, keyIsTooShortError)
+}
